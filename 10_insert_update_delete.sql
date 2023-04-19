0eb9dbb27f704994ccad5bdc3c48a74099a1a755
teataeho <rlaxogh9504@gmail.com>
2023-04-19 15:49:12 +0900

    2023.04.19 DML

--- 10_insert_update_delete.sql
+++ 10_insert_update_delete.sql
@@ -26,7 +26,7 @@ SELECT * FROM managers;
 DROP TABLE managers;
 
 -- INSERT (서브쿼리)
-INSERT INTo managers
+INSERT INTO managers
 (SELECT employee_id, first_name, job_id, hire_date
 FROM employees);
 
@@ -42,3 +42,45 @@ CTAS를 사용하면 제약 조건은 NOT NULL 말고는 복사되지 않습니다.
 제약조건은 업무 규칙을 지키는 데이터만 저장하고, 그렇지 않은 것들이
 DB에 저장되는 것을 방지하는 목적으로 사용합니다.
 */
+
+-- UPDATE를 진행할 때는 누구를 수정할 지 잘 지목해야 합니다.
+-- 그렇지 않으면 수정 대상이 테이블 전체로 지목됩니다.
+UPDATE emps SET salary = 30000;
+SELECT * FROM emps;
+ROLLBACK;
+
+UPDATE emps SET salary = 30000
+WHERE employee_id = 100;
+
+UPDATE emps SET salary = salary + salary * 0.1
+WHERE employee_id = 100;
+
+UPDATE emps SET
+phone_number = '010.4742.8917', manager_id = 102
+WHERE employee_id = 100;
+
+-- UPDATE (서브쿼리)
+UPDATE emps
+    SET (job_id, salary, manager_id) = 
+    (SELECT job_id, salary, manager_id
+    FROM emps
+    WHERE employee_id = 100
+    )
+WHERE employee_id = 101;
+
+-- DELETE
+DELETE FROM emps
+WHERE employee_id = 103;
+
+-- 사본테이블 생성
+CREATE TABLE depts AS (SELECT * FROM departments);
+SELECT * FROM depts;
+
+-- DELETE (서브쿼리)
+DELETE FROM emps
+WHERE department_id = (SELECT department_id FROM depts
+                        WHERE department_id = 100);
+                        
+DELETE FROM emps
+WHERE department_id = (SELECT department_id FROM depts
+                        WHERE department_name = 'IT');
